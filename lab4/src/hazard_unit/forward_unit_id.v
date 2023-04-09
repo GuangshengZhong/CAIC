6c21b7943da473ce395a8f5a6f3d84afc6ceaa72
GuangshengZhong <484362429@qq.com>
2023-04-09 11:26:54 +0800

    modified:   lab4/src/hazard_unit/forward_unit_id.v 	modified:   lab4/src/hazard_unit/hazard_detect_unit.v

--- lab4/src/hazard_unit/forward_unit_id.v
+++ lab4/src/hazard_unit/forward_unit_id.v
@@ -3,7 +3,12 @@ module Forward_Unit_Id(
     input reg_write_mem,
     input [4:0] rs1_id,rs2_id,rd_mem,
     input branch_id, jal_id, jalr_id,
-    output [1:0] rs1_fwd_id,rs2_fwd_id
+    output rs1_fwd_id,rs2_fwd_id
 )
-
+    always @(*) begin
+        if(branch_id&&(rd_mem!=5'b0)&&(rd_MEM == rs1_id)) rs1_fwd_id = 1'b1;
+        if(branch_id&&(rd_mem!=5'b0)&&(rd_MEM == rs1_id)) rs2_fwd_id = 1'b1;
+        if(jalr_id&&(rd_mem!=5'b0)&&(rd_MEM == rs1_id)) rs1_fwd_id = 1'b1;
+        //jal?
+    end
 endmodule
