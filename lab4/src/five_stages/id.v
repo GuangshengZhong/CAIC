9fa179c15b8e2b1406a28e1fc15e1f6bae33d5d0
GuangshengZhong <484362429@qq.com>
2023-04-24 11:28:20 +0800

    ram3再一次没问题了

--- lab4/src/five_stages/id.v
+++ lab4/src/five_stages/id.v
@@ -77,9 +77,10 @@ module ID_MODULE(
 
     wire [31:0] reg_write_data_chosen;
     wire [4:0] reg_write_addr;
-    assign reg_write_data_chosen = (rs1_fwd_id||rs2_fwd_id||stall_id) ? reg_write_data_mem : reg_write_data_wb;//加了stall之后好了！！
-    // assign reg_write_data_chosen = reg_write_data_wb;
-    assign reg_write_addr = rs1_fwd_id ? R_Addr1 : (rs2_fwd_id ? R_Addr2 : rd_wb);
+    // assign reg_write_data_chosen = (rs1_fwd_id||rs2_fwd_id) ? reg_write_data_mem : reg_write_data_wb;//加了stall之后好了！！
+    assign reg_write_data_chosen = reg_write_data_wb;
+    // assign reg_write_addr = rs1_fwd_id ? R_Addr1 : (rs2_fwd_id ? R_Addr2 : rd_wb);
+    assign reg_write_addr = rd_wb;
 
     RegFile ID_RegFile(
         .read_addr1(R_Addr1),
